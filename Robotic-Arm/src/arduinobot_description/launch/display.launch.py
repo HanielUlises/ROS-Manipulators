984900455869aed0410f7a35bb8a30cc9b987018
HanielUlises <hanielulises2003@gmail.com>
2026-06-05 22:45:03 -0600

    Launch files

--- Robotic-Arm/src/arduinobot_description/launch/display.launch.py
+++ Robotic-Arm/src/arduinobot_description/launch/display.launch.py
@@ -2,8 +2,19 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import Command
+from launch.substitutions import Command, LaunchConfiguration
 import os
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    model_arg = DeclareLaunchArgument(
+        name="model",
+        default_value=os.path.join(get_package_share_directory("arduinobot_description"), "urdf", "arduinobot.urdf.xacro"),
+        description="Absolute path to the robot URDF file"
+    )
+
+    robot_description = ParameterValue(Command(["xacro", LaunchConfiguration("model")]))
+
+    robot_state_publisher = Node(
+        package="robot_state"
+    )
